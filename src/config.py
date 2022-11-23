9ac65b65d1bec28daa93f604aac7812ee9404f8d
Tomas Toth <tomas.toth004@gmail.com>
2022-11-24 00:00:53 +0100

    added test db, fixed test of saving models, added provider_assets to runner.async_update_all_addresses

--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ class Config:
     eth_rpc_url = os.getenv("ETH_RPC_URL")
     etherescan_key = os.getenv("ETHERSCAN_KEY")
     db_url = os.getenv("DB_URL")
+    test_db_url = os.getenv("TEST_DB_URL")
     covalent_key = os.getenv("COVALENT_API_KEY")
     root_dir = os.path.dirname(os.path.abspath(__file__)).replace("src", "")
     test_data_dir = root_dir + "\\tests" + "\\test_data"
--- src/runner.py
+++ src/runner.py
@@ -1,20 +1,21 @@
 from sqlalchemy.ext import asyncio as sql_asyncio
 
-from src import debank
+from src import debank, spec
 from src.database import services
 
 
-async def async_update_all_addresses(session: sql_asyncio.AsyncSession) -> None:
+async def async_update_all_addresses(
+    session: sql_asyncio.AsyncSession, provide_assets: spec.AssetProvider | None = None
+) -> None:
     addresses_models = await services.async_find_all_addresses(session)
     addresses = [
         services.convert_address_model(address_model)
         for address_model in addresses_models
     ]
     debank_client = debank.Debank()
-    address_updates = [
-        await debank_client.async_get_assets_for_address(address)
-        for address in addresses
-    ]
+    if not provide_assets:
+        provide_assets = debank_client.async_get_assets_for_address
+    address_updates = [await provide_assets(address) for address in addresses]
     for i, address_update in enumerate(address_updates):
         address = addresses[i]
         for aggregated_asset in address_update.aggregated_assets:
