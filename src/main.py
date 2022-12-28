f17bfc78cb5994b1b218b2f4002c1d24f6417d44
Tomas Toth <tomas.toth004@gmail.com>
2022-12-28 13:50:33 +0100

    improved dockerfile, poetry (remove src dependency), fixed db async usage

--- src/main.py
+++ src/main.py
@@ -1,50 +1,53 @@
+import src  # noqa
 import asyncio
 import logging
+import time
 
 from apscheduler.schedulers import asyncio as asyncio_scheduler
 from apscheduler.triggers import cron
 from defi_common.database import db
 from sqlalchemy.ext import asyncio as sql_asyncio
 
-import src  # noqa
 from src import addresses, enums, runner
 
 
-async def async_run_executor(event_loop: asyncio.AbstractEventLoop) -> None:
-    scheduler = asyncio_scheduler.AsyncIOScheduler(event_loop=event_loop)
-    session = await db.get_session().__anext__()
-    scheduler.add_job(
-        runner.async_update_all_addresses,
-        kwargs={"session": session},
-        trigger=cron.CronTrigger.from_crontab("*/15 * * * *"),
-    )
-    session = await db.get_session().__anext__()
-    scheduler.add_job(
-        runner.async_run_address_ranking,
-        kwargs={"session": session, "time_type": enums.RunTimeType.HOUR},
-        trigger=cron.CronTrigger.from_crontab("1 * * * *"),
-    )
-    session = await db.get_session().__anext__()
-    scheduler.add_job(
-        runner.async_run_coin_change_ranking,
-        kwargs={"session": session, "time_type": enums.RunTimeType.HOUR},
-        trigger=cron.CronTrigger.from_crontab("1 * * * *"),
-    )
-    session = await db.get_session().__anext__()
-    scheduler.add_job(
-        runner.async_run_address_ranking,
-        kwargs={"session": session, "time_type": enums.RunTimeType.DAY},
-        trigger=cron.CronTrigger.from_crontab("1 0 * * *"),
-    )
+async def async_run_executor() -> None:
+    scheduler = asyncio_scheduler.AsyncIOScheduler()
+    session = db.get_session()
+    async with db.async_session() as session:
+        scheduler.add_job(
+            runner.async_update_all_addresses,
+            kwargs={"session": session},
+            trigger=cron.CronTrigger.from_crontab("*/15 * * * *"),
+        )
+    async with db.async_session() as session:
+        scheduler.add_job(
+            runner.async_run_address_ranking,
+            kwargs={"session": session, "time_type": enums.RunTimeType.HOUR},
+            trigger=cron.CronTrigger.from_crontab("1 * * * *"),
+        )
+    async with db.async_session() as session:
+        scheduler.add_job(
+            runner.async_run_coin_change_ranking,
+            kwargs={"session": session, "time_type": enums.RunTimeType.HOUR},
+            trigger=cron.CronTrigger.from_crontab("1 * * * *"),
+        )
+    async with db.async_session() as session:
+        scheduler.add_job(
+            runner.async_run_address_ranking,
+            kwargs={"session": session, "time_type": enums.RunTimeType.DAY},
+            trigger=cron.CronTrigger.from_crontab("1 0 * * *"),
+        )
     scheduler.start()
-    event_loop.run_forever()
+    # block main thread forever to let schedulers run
+    while True:
+        time.sleep(10)
 
 
-async def init_db() -> sql_asyncio.AsyncSession:
+async def init_db() -> None:
     await db.init_models()
     session = db.async_session()
     await addresses.async_save_addresses_from_all_providers(session)
-    return session
 
 
 def setup_logging() -> None:
@@ -60,6 +63,6 @@ async def run_updating(session: sql_asyncio.AsyncSession) -> None:
 
 if __name__ == "__main__":
     setup_logging()
-    event_loop = asyncio.get_event_loop()
-    session = event_loop.run_until_complete(init_db())
-    event_loop.run_until_complete(async_run_executor(session, event_loop))
+    event_loop = asyncio.new_event_loop()
+    event_loop.run_until_complete(init_db())
+    event_loop.run_until_complete(async_run_executor())
